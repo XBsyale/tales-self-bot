e70a5a99d7726b42746c47f86c66784669eb0a1d
XBsyale <firatmehmetefe477@gmail.com>
2025-04-04 19:30:08 +0300

    tales self bot

--- run.bat
+++ run.bat
@@ -1,28 +1,37 @@
 @echo off
 title Tales SelfBot - Otomatik Güncelleyici
 color 0a
-echo.
-echo [~] Tales SelfBot baslatiliyor...
-echo [~] Guncellemeler kontrol ediliyor...
+set RESTART_FLAG=0
 
-:: Güncelleme işlemi için Node'u çalıştır
+:start
+echo.
+echo [~] Tales SelfBot başlatılıyor...
+
+if "%RESTART_FLAG%"=="1" (
+    echo [~] Güncelleme sonrası yeniden başlatılıyor...
+    set RESTART_FLAG=0
+) else (
+    echo [~] Güncellemeler kontrol ediliyor...
+)
+
+:: Güncelleme kontrolü yap
 node -e "require('./updater').checkUpdates().then(updated=>{process.exit(updated?1:0)})"
 
-:: Güncelleme yapıldıysa (exit code 1) scripti yeniden başlat
+:: Güncelleme yapıldıysa (exit code 1) flag'i ayarla ve yeniden başlat
 if %errorlevel% equ 1 (
-    echo [~] Guncelleme tamamlandi, yeniden baslatiliyor...
+    set RESTART_FLAG=1
+    echo [~] Güncelleme tamamlandı, yeniden başlatılıyor...
     timeout /t 3 /nobreak >nul
-    start "" "%~dp0run.bat"
-    exit
+    goto start
 )
 
 :: Ana uygulamayı başlat
-echo [~] Main uygulamasi baslatiliyor...
+echo [~] Ana uygulama başlatılıyor...
 node main.js
 
 :: Hata durumunda konsolu açık tut
 if %errorlevel% neq 0 (
     echo.
-    echo [!] Hata: Uygulamadan cikis kodu %errorlevel%
+    echo [!] Hata: Uygulamadan çıkış kodu %errorlevel%
     pause
 )

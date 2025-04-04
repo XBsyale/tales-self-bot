4e73255e927a35c6b9ca1244c8618b6be5f68473
XBsyale <firatmehmetefe477@gmail.com>
2025-04-04 19:27:09 +0300

    tales self bot

--- run.bat
+++ run.bat
@@ -1,4 +1,28 @@
 @echo off
 title Tales SelfBot - Otomatik Güncelleyici
+color 0a
+echo.
+echo [~] Tales SelfBot baslatiliyor...
+echo [~] Guncellemeler kontrol ediliyor...
+
+:: Güncelleme işlemi için Node'u çalıştır
+node -e "require('./updater').checkUpdates().then(updated=>{process.exit(updated?1:0)})"
+
+:: Güncelleme yapıldıysa (exit code 1) scripti yeniden başlat
+if %errorlevel% equ 1 (
+    echo [~] Guncelleme tamamlandi, yeniden baslatiliyor...
+    timeout /t 3 /nobreak >nul
+    start "" "%~dp0run.bat"
+    exit
+)
+
+:: Ana uygulamayı başlat
+echo [~] Main uygulamasi baslatiliyor...
 node main.js
-if %errorlevel% neq 0 pause
+
+:: Hata durumunda konsolu açık tut
+if %errorlevel% neq 0 (
+    echo.
+    echo [!] Hata: Uygulamadan cikis kodu %errorlevel%
+    pause
+)
